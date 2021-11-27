2fc183406880c138629617b953cc3726722f8383
Ilya Fomenko <4webmaster4@gmail.com>
2021-11-27 19:34:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -431,8 +431,8 @@ def main():
                 
         st.subheader('Upload camera calibration photos :')
         ###########################################################################################
-        CalibFiles = st.file_uploader("", type=['png', 'jpg','tif','tiff','TIF','TIFF', 'jpeg','bmp','webp','pfm','sr','ras','exr','hdr','pic'], accept_multiple_files=True)
-        
+        #CalibFiles = st.file_uploader("", type=['png', 'jpg','tif','tiff','TIF','TIFF', 'jpeg','bmp','webp','pfm','sr','ras','exr','hdr','pic'], accept_multiple_files=True)
+        CalibFiles = st.file_uploader("", type=['jpg', 'jpeg'], accept_multiple_files=True)
         if CalibFiles :
             CaseLength = st.number_input(label='Square width', min_value=0.00, max_value=None, value=24.40, help='The width in mm of the calibration board Square')                
             MarkerLength = st.number_input(label='Marker width', min_value=0.00, max_value=None, value=12.30, help='The width in mm of the calibration board Marker')                
